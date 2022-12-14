e4f688d84a80e48a3c500cb7312ab1fec96356e4
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2022-12-14 10:34:20 +0200

    editing imgs path

--- js/AboutUsImgs.js
+++ js/AboutUsImgs.js
@@ -2,12 +2,13 @@ let imgs = [];
 for(let i=1; i<=5; i++){
     imgs.push(`clr-${i}.svg`)
 }
+
 let aboutUsImg = document.querySelector("#about-us .img-box img")
 let AboutUsImage = (function(){
 
     let setImg = (clr)=>{
         let img = imgs.find((img)=> img.includes(clr));
-        aboutUsImg.src= `../imgs/colored-imgs/${img}`;
+        aboutUsImg.src= `/imgs/colored-imgs/${img}`;
     }
 
     return {
